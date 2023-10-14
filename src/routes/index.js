d3bd29f015caec072ac1be248a2f75b7e48ec515
Juliane Pires <mpiresjuliane@outlook.com>
2023-10-14 19:31:52 -0300

    fix: corrige utilização do controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const bookController = require("../controllers/BookController");
 
 //TODO: Criar rotas da aplicação
-router.get("/", controller.getBooks);
+router.get("/", bookController.getBooks);
 
-router.get("/:id", controller.getBookById);
+router.get("/:id", bookController.getBookById);
 
 module.exports = router;
